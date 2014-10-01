e3de51dab5c0af32bd578003dcc17e17c62f4523
Roderick Lagunas <rdcklinux@gmail.com>
2014-10-01 13:29:17 -0300

    improves
    improve exception control and add auto executable feature

--- app/manager/console.py
+++ app/manager/console.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 __author__ = 'Roderick Lagunas'
 
@@ -13,30 +14,32 @@ if __name__ == "__main__":
     from app.manager import module
 
     try:
-        command.Core(module, sys.argv)
+        try:
+            command.Core(module, sys.argv)
+        except (IndexError, AttributeError), e:
+            command.Core(module)
+            exit()
+
         _names = sys.argv[1].split(':')
-        _attr = getattr(module, _names[0])
+        _attr = None
+        try:
+            _attr = getattr(module, _names[0])
+        except AttributeError, e:
+            print "\n" + 'Modulo %s no disponible.' % sys.argv[1] + ' - ' + e.message + "\n"
+            exit()
+
         _class = getattr(_attr, _names[1].capitalize())
 
-        try:
-            import re
-            regex = re.compile('^\-{1,2}([\w|\d]+)(=(.+))?$')
-            args = dict()
-            for i, arg in enumerate(sys.argv[2:]):
-                result = regex.match(arg)
-                if result:
-                    args[result.group(1)] = args[i] = (result.group(3) if result.group(3) else True)
-                else:
-                    args[i] = arg
-            _class().main(args)
-        except AttributeError, e:
-            print 'Implementacion de %s invalida.' % sys.argv[1] + ' message: ' + str(e)
-
-    except AttributeError, e:
-        print 'Modulo %s no disponible.' % sys.argv[1] + ' - ' + e.message
-
-    except IndexError:
-        command.Core(module)
+        import re
+        regex = re.compile('^\-{1,2}([\w|\d]+)(=(.+))?$')
+        args = dict()
+        for i, arg in enumerate(sys.argv[2:]):
+            result = regex.match(arg)
+            if result:
+                args[result.group(1)] = args[i] = (result.group(3) if result.group(3) else True)
+            else:
+                args[i] = arg
+        _class().main(args)
 
     except exception.FinishException:
         print command.Core.Color.sys
